077824598652198c8c8001e21329ba845af6b94b
peanutDD <www.smoking.sexy@gmail.com>
2018-05-26 13:12:42 +0800

    upload-async

--- file-upload-async-index.js
+++ file-upload-async-index.js
@@ -6,15 +6,14 @@ const views = require('koa-views')
 const path = require('path')
 const convert = require('koa-convert')
 const static = require('koa-static')
-const {
-              uploadFile
-} = require('./util/upload')
+const {uploadFile} = require('./file-upload-async')
 
 const app = new Koa()
 
-/**
- * 使用第三方中间件 start 
- */
+/*=============================================
+=            使用第三方中间件 start            =
+=============================================*/
+
 app.use(views(path.join(__dirname, './view'), {
     extension: 'ejs'
 }));
@@ -27,14 +26,13 @@ app.use(convert(static(
     path.join(__dirname, staticPath)
 )))
 
-/**
- * 使用第三方中间件 end 
- */
+/*=====      End of 使用第三方中间件      ======*/
 
 app.use( async ( ctx ) => {
-  if ( ctx.method === 'GET' ) {
+  if ( ctx.url === '/' && ctx.method === 'GET' ) {
     let title = 'upload pic async';
-    await ctx.render('index', {
+    console.log('666666666');
+    await ctx.render('upload-async', {
       title,
     });
   } else if ( ctx.url === '/api/picture/upload.json' && ctx.method === 'POST' ) {
@@ -55,37 +53,6 @@ app.use( async ( ctx ) => {
 
 })
 
-app.listen(3000, () => {
-  console.log('[demo] upload-pic-async is starting at port 3000')
-})
-
-app.use(asycn (ctx) => {
-  if(ctx.method ==== 'GET') {
-
-    let title = 'upload pic async'
-    await ctx.render('index', {
-      title,
-    })
-    
-  } else if(ctx.url === '/api/picture/upload.json' && ctx.method === 'POST'){
-
-    // 上传文件请求处理
-    let result = { success: false }
-    let serverFilePath = path.join( __dirname, 'static/image')
-
-    // 上传文件事件
-    result = await uploadFile(ctx, {
-      fileType: 'album',
-      path: serverFilePath
-    })
-    ctx.body = result
-    
-  } else {
-    // 其他请求显示404
-    ctx.body = '<h1>404！！！ o(╯□╰)o</h1>'
-  }
-})
-
-app.listen(3000, () => {
-  console.log('[demo] upload-pic-async is starting at port 3000')
+app.listen(4000, () => {
+  console.log('[demo] upload-pic-async is starting at port 4000')
 })
