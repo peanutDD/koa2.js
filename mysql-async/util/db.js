add7db6766865f535b494e80752910bcbcb15c83
peanutDD <www.smoking.sexy@gmail.com>
2018-05-29 11:21:04 +0800

    mysql

---
+++ mysql-async/util/db.js
@@ -0,0 +1,36 @@
+
+/* jshint esversion: 6 */
+
+
+const mysql = require('mysql');
+
+const pool = mysql.createPool({
+  host: '127.0.0.1',
+  user     :  'root',
+  password :  '2486',
+  database :  'koa_mysql'
+});
+
+let query = function (sql, values) {
+  return new Promise((resolve, reject) => {
+      pool.getConnection(function (err, connection) {
+        if (err) {
+          reject(err);
+        } else {
+          connection.query(sql, values, (err, rows) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(rows);
+            }
+
+            connection.release();
+          });
+        }
+      });
+  });
+};
+
+module.exports = {
+  query
+};
