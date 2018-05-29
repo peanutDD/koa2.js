4be294d83eb739ac6662e3d74aa43f54dd088c55
peanutDD <www.smoking.sexy@gmail.com>
2018-05-29 11:22:13 +0800

    koa-project

---
+++ koa-project/config.js
@@ -0,0 +1,16 @@
+/* jshint esversion: 6 */
+
+const config = {
+
+  port: 3001,
+  database: {
+    DATABASE: 'koa_project',
+    USERNAME: 'root',
+    PASSWORD: '2486',
+    PORT: '3306',
+    HOST: 'localhost'
+  }
+
+};
+
+module.exports = config;
---
+++ koa-project/readme.md
@@ -0,0 +1,19 @@
+# koa-project
+
+![koa-config](koa-config.png)
+
+## 分层设计
+
+### 后端代码目录
+
+![server](server.png)
+
+#### 路由目录
+
+![routers](routers.png)
+
+- [koa-project](#koa-project)
+  - [分层设计](#)
+    - [后端代码目录](#)
+      - [路由目录](#)
+
